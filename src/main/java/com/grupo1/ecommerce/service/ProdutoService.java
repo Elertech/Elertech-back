24474ad36f1bfb743531428d72e30f688524cca0
ViniciusCampanholi <viniciuscampanholi@hotmail.com>
2022-07-25 12:34:11 -0300

    Melhorias de desempenho

--- src/main/java/com/grupo1/ecommerce/service/ProdutoService.java
+++ src/main/java/com/grupo1/ecommerce/service/ProdutoService.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.grupo1.ecommerce.model.Categoria;
 import com.grupo1.ecommerce.model.Produto;
+import com.grupo1.ecommerce.repository.CategoriaRepository;
 import com.grupo1.ecommerce.repository.ProdutoRepository;
 
 @Service
@@ -16,8 +18,19 @@ public class ProdutoService {
     @Autowired
     private ProdutoRepository produtoRepository;
 
-    public ResponseEntity<Produto> cadastrarNovoProduto(Produto produto){
-		  return ResponseEntity.status(HttpStatus.CREATED).body(produtoRepository.save(produto));
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    public ResponseEntity<Produto> cadastrarNovoProduto(Produto produto, Long idCategoria){
+        Categoria categoria = categoriaRepository.findById(idCategoria).get();
+        produto.setCategoria(categoria);
+	    return ResponseEntity.status(HttpStatus.CREATED).body(produtoRepository.save(produto));
+    }
+
+    public ResponseEntity<Produto> atualizarProduto(Produto produto, Long idCategoria){
+        Categoria categoria = categoriaRepository.findById(idCategoria).get();
+        produto.setCategoria(categoria);
+        return ResponseEntity.ok(produtoRepository.save(produto));
     }
 
     public List<Produto> buscarProdutoPorCategoria(Long idCategoria){
